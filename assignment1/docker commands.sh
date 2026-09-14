efb2b558eae2b890b2099efc78ab4ba551a313c9
Chase Vining <crvining27@gmail.com>
2026-09-14 12:25:29 -0500

    Complete Question 2 volume mounting

--- assignment1/docker commands.sh
+++ assignment1/docker commands.sh
@@ -2,6 +2,10 @@
 docker build -t linear-regression-app:latest .
 docker tag linear-regression-app:latest linear-regression-app:v0.1.1
 docker tag linear-regression-app:latest linear-regression-app:v0.1.2
+docker images
 docker login
 docker tag linear-regression-app:latest chaserv27/linear-regression-app:latest
 docker push chaserv27/linear-regression-app:latest
+# Question 2
+docker build -t linear-regression-volume:latest .
+docker run --rm --volume "$(pwd)/data:/home/iem4723/data" linear-regression-volume:latest
